cffc6aed22ea0293328aef3b9c468a872437150b
chandu078 <chandudyavanapelli03@gmail.com>
2025-05-06 10:09:53 +0530

    waffle: Sync with device tree
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- waffle-vendor.mk
+++ waffle-vendor.mk
@@ -6,7 +6,6 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/oneplus/waffle
 
 PRODUCT_COPY_FILES += \
-    vendor/oneplus/waffle/proprietary/odm/etc/aac_richtap.config:$(TARGET_COPY_OUT_ODM)/etc/aac_richtap.config \
     vendor/oneplus/waffle/proprietary/odm/etc/acdbdata/Audio_Parameter_Version.txt:$(TARGET_COPY_OUT_ODM)/etc/acdbdata/Audio_Parameter_Version.txt \
     vendor/oneplus/waffle/proprietary/odm/etc/acdbdata/FTM/acdb_cal.acdb:$(TARGET_COPY_OUT_ODM)/etc/acdbdata/FTM/acdb_cal.acdb \
     vendor/oneplus/waffle/proprietary/odm/etc/acdbdata/FTM/workspaceFileXml.qwsp:$(TARGET_COPY_OUT_ODM)/etc/acdbdata/FTM/workspaceFileXml.qwsp \
